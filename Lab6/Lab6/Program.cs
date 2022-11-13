4bacf8f59975aec45a30e1e05b1ccb75987d5649
justintime2003 <justin.m0103@gmail.com>
2022-11-12 21:50:38 -0800

    Finished Lab6
    accompanied with unit tests: CalculusTests.cs

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -1,2 +1,121 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Student name: Justin The
+//Student number: 48875413
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab6
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //You could mainly use unit tests for testing.       
+            
+        }
+    }
+    public class Calculus
+    {
+        // Delegate for any function whose definite integral or derivative we want to calculate
+        public delegate double Function(double x); //accessor
+
+        /// <summary>
+        /// Finds the definite integral of f between a and b using 
+        /// the rectangular method for a given n (sub-intervals).
+        /// </summary>
+        /// <param name="f">A delegate representing the function f.</param>
+        /// <param name="a">Lower limit of the integral.</param>
+        /// <param name="b">upper limit of the integral.</param>
+        /// <param name="n">The number of subintervals between a and b for the rectangular method.</param>
+        /// <returns>Returns the calculated integral value.</returns>
+        /// <exception cref="ArgumentException">
+        /// thrown if n is not a positive number.
+        /// </exception>
+        public static double RectangularMethod(Function f, double a, double b, int n)
+        {
+            if(n <= 0)
+            {
+                throw new ArgumentException("n cannot be negative"); 
+            }
+
+            double deltaX = ( b - a) / n;
+            double result = 0;  
+
+            for (int i = 1; i <= n; i++)
+            {
+                double inputFunc = a + (i * deltaX); 
+                 result += f(inputFunc) * deltaX; //riemann sum formula
+            }
+            return result; 
+        }
+
+        /// <summary>
+        /// Finds the definite integral adaptively by starting from 
+        /// a seed value for n (# of sub-intervals), until the desired
+        /// accuracy is achieved.
+        /// This method uses RectangularMethod() in its implementation.
+        /// </summary>
+        /// <param name="f">A delegate representing the function f.</param>
+        /// <param name="a">Lower limit of the integral.</param>
+        /// <param name="b">upper limit of the integral.</param>
+        /// <param name="SeedForN">The starting (seed) n for the rectangular method.</param>
+        /// <param name="epsilon">The desired accuracy.</param>
+        /// <returns>Returns the calculated integral value. 
+        /// If an acceptable result is not found, returns double.NaN.</returns>
+        ///<exception cref="ArgumentException">
+        /// thrown if epsilon is not a positive number.
+        /// </exception>
+        public static double AdaptiveRectangularMethod(Function f, double a, double b, int SeedForN, double epsilon)
+        {
+            int nNaut = SeedForN;
+            double result = 0; 
+            int MaxIterations = 10;
+            int count;  
+
+            if(epsilon <= 0)
+            {
+                throw new ArgumentException("Epsilon must be a positive number");
+            }
+
+            for(count = 0; count < MaxIterations; count++)
+            {
+                int nOne = 2 * nNaut;
+                double firstPoint = RectangularMethod(f, a, b, nNaut);
+                double secondPoint = RectangularMethod(f, a, b, nOne); 
+
+                if (Math.Abs(firstPoint - secondPoint) < epsilon) //calculate accuracy of each subinterval
+                {
+                    result = secondPoint; 
+                    return result; 
+                }
+                else
+                {
+                    nNaut *= 2; 
+                }
+            }
+
+            if(count == MaxIterations)
+            {
+                return double.NaN;
+            }
+            
+            return result; 
+        }
+
+        /// <summary>
+        /// Finds the derivative of f() at the given x.
+        /// </summary>
+        /// <param name="f">A delegate representing the function f.</param>
+        /// <param name="x">The point at which to calculate the derivative.</param>
+        /// <param name="h">The h used for the central difference method.</param>
+        /// <returns>Returns the calculated derivative value. </returns>
+        public static double CentralDifferenceDerivative(Function f, double x, double h)
+        {
+            double numerator = f(x - (2 * h)) - 8 * f(x - h) + 8 * f(x + h) - f(x + (2 * h));
+            double denominator = 12 * h;
+
+            return numerator / denominator; 
+        }
+    }
+}
---
+++ Lab6/Lab6Tests/CalculusTests.cs
@@ -0,0 +1,138 @@
+﻿//Student name: Justin The
+//Student number: 48875413
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab6.Tests
+{
+    [TestClass()]
+    public class CalculusTests
+    {
+        //Put a helper method below for each of the f(x) that you have a test case on
+        //Each of these helper methods must take one parameter (type double) and must have a double return type
+        double TestFunction1(double x) //a linear function
+        {
+            return x + 2;
+        }
+        double TestFunction2(double x) //the cos(x) - x^3 function
+        {
+            return Math.Cos(x) - x * x * x;
+        }
+        double TestFunction3(double x)
+        {
+            return Math.Pow(x, 2); 
+        }
+        double TestFunction4(double x)
+        {
+            return 1 / Math.Pow(x, 2); 
+        }
+        double TestFunction5(double x)
+        {
+            return Math.Sin(x); 
+        }
+
+        [TestMethod()]
+        public void DefiniteIntegralTest1()
+        {
+            int n = 500;
+            double a = 0;
+            double b = 1;
+
+            double result = Calculus.RectangularMethod(TestFunction1, a, b, n);
+            Assert.AreEqual(2.5, result, 0.01);
+        }
+        [TestMethod()]
+        public void DefiniteIntegralTest2()
+        {
+            int n = 500;
+            double a = 0;
+            double b = 1;
+
+            double result = Calculus.RectangularMethod(TestFunction3, a, b, n);
+            Assert.AreEqual(0.333333333, result, 0.01);
+        }
+        [TestMethod()]
+        public void DefiniteIntegralTest3()
+        {
+            int n = 500;
+            double a = 2;
+            double b = 3;
+
+            double result = Calculus.RectangularMethod(TestFunction4, a, b, n);
+            Assert.AreEqual(0.16666666666, result, 0.01);
+        }
+        [TestMethod()]
+        public void DefiniteIntegralTest4()
+        {
+            int n = 500;
+            double a = 0;
+            double b = Math.PI / 2;
+
+            double result = Calculus.RectangularMethod(TestFunction5, a, b, n);
+            Assert.AreEqual(1, result, 1);
+        }
+        [TestMethod()]
+        public void NumericalDifferentiationTest()
+        {
+            double x = 3;
+            double h = 1;
+
+           double result = Calculus.CentralDifferenceDerivative(TestFunction1, x, h);
+            Assert.AreEqual(1, result, 0.001); 
+        } 
+        [TestMethod()]
+        public void NumericalDifferentaitionTest2()
+        {
+            double x = 1;
+            double h = 1;
+
+           double result = Calculus.CentralDifferenceDerivative(TestFunction3, x, h);
+            Assert.AreEqual(2, result, 0.001);
+        } 
+        [TestMethod()]
+        public void AdaptiveIntegralTest1()
+        {
+            double a = 0;
+            double b = 1;
+            int seed = 10;
+            double epsilon = 0.001; 
+
+           double result = Calculus.AdaptiveRectangularMethod(TestFunction1, a, b, seed, epsilon);
+            Assert.AreEqual(2.5, result, 0.01);
+        }
+        [TestMethod()]
+        public void AdaptiveIntegralTest2()
+        {
+            double a = 0;
+            double b = 1;
+            int seed = 20;
+            double epsilon = 0.001;  
+
+           double result = Calculus.AdaptiveRectangularMethod(TestFunction3, a, b, seed, epsilon);
+            Assert.AreEqual(0.333333, result, 0.01);
+        }
+
+       
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ExceptionTest1()
+        {
+            Calculus.RectangularMethod(TestFunction2, 0, 1, 0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void ExceptionTest2()
+        {
+            Calculus.AdaptiveRectangularMethod(TestFunction3, 0, 1, 1, 0);
+        }
+    }
+}
