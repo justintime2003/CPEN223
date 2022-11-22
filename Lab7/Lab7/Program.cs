e0425d7e3e5e9558903a4b2a4f522c85329f596f
justintime2003 <justin.m0103@gmail.com>
2022-11-22 08:19:32 -0800

    Finished Lab7
    Recursion and RecursionTests

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,103 @@
+﻿//Student name: Justin The
+//Student number: 48875413
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab7
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            //You could mainly use unit tests for testing.       
+        }
+    } 
+    public class Recursion 
+    {
+        /// </summary>
+        /// <param name="stack">The stack whose reverse we want to obtain. It must not be modified when method returns.</param>
+        /// <param name="reversed">A stack containing the reverse of stack. Originally, reversed is an empty stack.</param>
+        public static void Reverse(Stack<int> stack, Stack<int> reversed)
+        {
+            if (stack.Count == 0)
+            {
+                foreach (int item in reversed)
+                {
+                    stack.Push(item);
+                }
+                //Console.WriteLine("Reversed Stack: "); 
+                foreach (int item in reversed) 
+                {
+                    Console.WriteLine(item);
+                }
+                //Console.WriteLine("Original Stack: "); 
+                //foreach (int item in stack) 
+                //{
+                //    Console.WriteLine(item);
+                //}
+            }
+            else
+            {
+                reversed.Push(stack.Pop());
+                Reverse(stack, reversed);
+            }
+
+        }
+
+        /// <summary>
+        /// Determines if sub is a subset of set.
+        /// </summary>
+        /// <param name="sub">A list representing a set of integers. Duplicate elements are allowed. sub may be modified.</param>
+        /// <param name="set">A list representing a set of integers. Duplicate elements are allowed.</param>
+        /// <returns>True if sub is a subset of set, false otherwise.</returns>
+        public static bool IsASubset(List<int> sub, List<int> set)
+        {
+            if (sub.Count == 0)
+            {
+                return true;
+            }
+            if (!set.Contains(sub[0]))
+            {
+                return false; 
+            }
+            if (sub.Contains(sub[0])) //removes duplicates in sub
+            {
+                sub.Remove(sub[sub.IndexOf(sub[0])]);
+                return IsASubset(sub, set); 
+            }
+            else
+            {
+                set.Remove(set[set.IndexOf(sub[0])]);
+                sub.Remove(sub[0]);
+                return IsASubset(sub, set); 
+            }
+        }
+
+        /// <summary>
+        /// Finds and returns the binary equivalent of the positive integer num.
+        /// </summary>
+        /// <param name="num">The integer whose binary equivalent to find. num is greater than 0.</param>
+        /// <returns>A string containing the binary equivalent of num.</returns>
+        public static string ToBinary(int num)
+        {
+            if (num == 0) //base case, return nothing 
+            {
+                return "";
+            }
+            if (num % 2 == 0) //check if quotient is even
+            {
+                return ToBinary(num / 2) + "0";
+            }
+            else
+            {
+                return ToBinary(num / 2) + "1";
+            }
+        }
+    }
+}
---
+++ Lab7/Lab7Tests/RecursionTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.Tests
+{
+    [TestClass()]
+    public class RecursionTests
+    {
+        [TestMethod()]
+        public void ReverseTest1()
+        {
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(3);
+            stack1.Push(2);
+            stack1.Push(1);
+            Stack<int> stack2 = new Stack<int>(); 
+            Recursion.Reverse(stack1, stack2);
+            Assert.AreEqual(3, stack2.Pop());
+            Assert.AreEqual(2, stack2.Pop());
+            Assert.AreEqual(1, stack2.Pop());
+        }
+        [TestMethod()]
+        public void ReverseTest2()
+        {
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(0);
+            stack1.Push(-1);
+            stack1.Push(1);
+            Stack<int> stack2 = new Stack<int>(); 
+            Recursion.Reverse(stack1, stack2);
+            Assert.AreEqual(0, stack2.Pop());
+            Assert.AreEqual(-1, stack2.Pop());
+            Assert.AreEqual(1, stack2.Pop());
+        }
+        [TestMethod()]
+        public void ReverseTest3()
+        {
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(10000);
+            stack1.Push(-10000);
+            Stack<int> stack2 = new Stack<int>(); 
+            Recursion.Reverse(stack1, stack2);
+            Assert.AreEqual(10000, stack2.Pop());
+            Assert.AreEqual(-10000, stack2.Pop());
+        }
+
+        [TestMethod()]
+        public void SubsetTest1()
+        {
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> {1, 2, 3};
+
+            Assert.IsTrue(Recursion.IsASubset(list2, list1)); 
+        }
+        [TestMethod()]
+        public void SubsetTest2()
+        {
+            List<int> list1 = new List<int> {0, 1, -1};
+            List<int> list2 = new List<int> {1, -1};
+
+            Assert.IsTrue(Recursion.IsASubset(list2, list1)); 
+        }
+        [TestMethod()]
+        public void SubsetTest3()
+        {
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> {};
+
+            Assert.IsTrue(Recursion.IsASubset(list2, list1)); 
+        }
+        [TestMethod()]
+        public void SubsetTest4()
+        {
+            List<int> list1 = new List<int> { 1, 2, 3, 4 };
+            List<int> list2 = new List<int> {1, 6};
+
+            Assert.IsFalse(Recursion.IsASubset(list2, list1)); 
+        }
+        [TestMethod()]
+        public void SubsetTest5()
+        {
+            List<int> list1 = new List<int> {1, 2, 4};
+            List<int> list2 = new List<int> {2, 2, 2, 2};
+
+            Assert.IsTrue(Recursion.IsASubset(list2, list1)); 
+        }
+        [TestMethod()]
+        public void SubsetTest6()
+        {
+            List<int> list1 = new List<int> {1, 2, 4};
+            List<int> list2 = new List<int> {2, 2, 3};
+
+            Assert.IsFalse(Recursion.IsASubset(list2, list1)); 
+        }
+
+        [TestMethod()]
+        public void ToBinaryTest1()
+        {
+            Assert.AreEqual("1111011", Recursion.ToBinary(123)); 
+        }
+        [TestMethod()]
+        public void ToBinaryTest2()
+        {
+            Assert.AreEqual("111", Recursion.ToBinary(7)); 
+        }
+        [TestMethod()]
+        public void ToBinaryTest3()
+        {
+            Assert.AreEqual("10001", Recursion.ToBinary(17)); 
+        }
+    }
+}
