e63759656027656714e9b53ad7f31ad2b895ba64
justintime2003 <justin.m0103@gmail.com>
2022-11-28 15:16:00 -0800

    Lab 8
    Template code

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,57 @@
+﻿//Student name: Justin The
+//Student number: 48875413
+
+using System;
+
+namespace Lab8
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            //You could mainly use unit tests for testing.       
+        }
+
+    }
+    public class RootFinding
+    {
+        // Delegate for any function whose root we want to find
+        public delegate double Function(double x);
+
+        /// <summary>
+        /// Finds a root of f() using the Bisection method.
+        /// The cap for the # of attempted iterations is 300.
+        /// </summary>
+        /// <param name="f">A delegate representing the function f to find the root of.</param>
+        /// <param name="a">Left side of (a, b) that brackets a root.</param>
+        /// <param name="b">Right side of (a, b) that brackets a root. b is greater than a. </param>
+        /// <param name="epsilon">The desired accuracy.</param>
+        /// <returns>Returns the calculated root. If a root cannot be found, double.NaN is returned.</returns>
+        /// <exception cref="ArgumentException">
+        /// thrown if f(a)*f(b) is positive or 
+        ///           epsilon is less than or equal to 0. 
+        /// </exception>
+        public static double Bisection(Function f, double a, double b, double epsilon)
+        {
+
+        }
+
+        /// <summary>
+        /// Finds a root of f() using the Secant method.
+        /// </summary>
+        /// <param name="f">A delegate representing the function f to find the root of.</param>
+        /// <param name="x0">Initial guess x0.</param>
+        /// <param name="x1">Initial guess x1 where x1 is greater than x0. </param>
+        /// <param name="epsilon">The desired accuracy.</param>
+        /// <returns>Returns the calculated root. </returns>
+        /// <exception cref="ArgumentException">
+        /// thrown if x1 is not greater than x0 or 
+        ///        epsilon is less than or equal 0.
+        /// </exception>
+        public static double Secant(Function f, double x0, double x1, double epsilon)
+        {
+
+        }
+    }
+}
---
+++ Lab8/Lab8Tests/RootFindingTests.cs
@@ -0,0 +1,45 @@
+﻿//Student Name: Justin The
+//Student Number: 48875413
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8.Tests
+{
+    [TestClass()]
+    public class RootFindingTests
+    {
+        //Put a helper method below for each of the f(x) that you have a test case on
+        //Each of these helper methods must take one parameter (type double) and must have a double return type
+        double TestFunction1(double x) //a quadratic equation
+        {
+            return x * x + (-3) * x + 2;
+        }
+        double TestFunction2(double x) //the cos(x) - x^3 equation
+        {
+            return Math.Cos(x) - x * x * x;
+        }
+
+        [TestMethod()]
+        public void BisectionTest1()
+        {
+            double epsilon = 0.0001;
+            double a = 0.5;
+            double b = 1.5;
+            double result = RootFinding.Bisection(TestFunction1, a, b, epsilon);
+            Assert.AreEqual(1, result, epsilon);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void BisectionTest2()
+        {
+            RootFinding.Bisection(TestFunction2, 0.8, 0.85, 0.0001);
+        }
+    }
+}
